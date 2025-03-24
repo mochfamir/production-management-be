e77e9077ed02d498596050434fadeca787fae08b
Mochamad Faishal Amir <mochamad.faishal@efishery.com>
2025-03-24 14:37:16 +0700

    feat: keep supabase

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma/prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getHello() {
+    // Keep Supabase not paused
+    const data = await this.prisma.user.count();
+    console.log(data);
     return 'Hello World!';
   }
 }
